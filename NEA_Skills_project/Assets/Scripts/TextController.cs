9df06182d99589d5fb548883efca44fe3f355ee2
Livelyharry17 <Livelyharry17@gmail.com>
2024-09-04 18:47:27 +0100

    End of Task 3

---
+++ NEA_Skills_project/Assets/Scripts/TextController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TextController : MonoBehaviour
+{
+    public TextMeshPro mushroomCounter;
+    GameObject[] totalMushrooms;   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        totalMushrooms = GameObject.FindGameObjectsWithTag("MushroomRed");
+        mushroomCounter.text = "Mushrooms: " + totalMushrooms.Length.ToString();
+    }
+}
