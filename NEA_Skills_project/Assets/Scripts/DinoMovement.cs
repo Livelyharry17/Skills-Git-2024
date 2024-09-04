1f61710969a00f49e0a3b76d2f4489b37ed7f458
Livelyharry17 <Livelyharry17@gmail.com>
2024-09-04 17:44:30 +0100

    End of Task 2

---
+++ NEA_Skills_project/Assets/Scripts/DinoMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class DinoMovement : MonoBehaviour
+{
+    float speed = 0f;
+    Rigidbody2D rb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 5.1f;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.LeftArrow)) 
+        {
+        transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * speed,0,0);
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            rb.AddForce(Vector2.up * 300);
+        }
+    }
+    void resetPosition()
+    {
+        transform.SetPositionAndRotation( new Vector3 (-0.8208882f, 0.7700498f, 0), Quaternion.identity );
+    }
+}
---
+++ NEA_Skills_project/Assets/Scripts/rock.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rock : MonoBehaviour
+{
+    GameObject Dino;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Dino = GameObject.FindGameObjectWithTag("Player");  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log("You rock my world");
+        Dino.SendMessage("resetPosition");
+    }
+}
