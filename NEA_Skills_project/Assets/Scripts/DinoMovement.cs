037dd096dda828f0d323b03b17546a3d6a5f8e85
Livelyharry17 <Livelyharry17@gmail.com>
2024-09-11 17:26:31 +0100

    End of Task 4

--- NEA_Skills_project/Assets/Scripts/DinoMovement.cs
+++ NEA_Skills_project/Assets/Scripts/DinoMovement.cs
@@ -2,17 +2,22 @@
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DinoMovement : MonoBehaviour
 {
     float speed = 0f;
     Rigidbody2D rb;
+    public int lives = 0;
+    bool vulnerable = false;
     
     // Start is called before the first frame update
     void Start()
     {
         speed = 5.1f;
         rb = GetComponent<Rigidbody2D>();
+        lives = 2;
+        vulnerable = false;
     }
 
     // Update is called once per frame
@@ -29,6 +34,40 @@ public class DinoMovement : MonoBehaviour
     }
     void resetPosition()
     {
+        setLives();
         transform.SetPositionAndRotation( new Vector3 (-0.8208882f, 0.7700498f, 0), Quaternion.identity );
+    }
+
+    public void setLives()
+    {
+        lives -= 1;
+        if (lives < 0 || vulnerable == true)
+        {
+            Debug.Log("End of Game");
+            SceneManager.LoadScene("Lost");
+        }
+    }
+    IEnumerator VulnerableDeBuff()
+    {
+        yield return new WaitForSeconds(5f);
+        vulnerable = false;
+        Debug.Log("vulnerable = false!");
+
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "MushroomPink")
+        {
+            lives += 1;
+            Destroy(other.gameObject);
+        }
+        else if (other.gameObject.tag == "vulnerable")
+        {
+            Destroy(other.gameObject);
+            vulnerable = true;
+            Debug.Log("vulnerable = true!");
+            StartCoroutine("VulnerableDeBuff");
+        }
+        
     }
 }
--- NEA_Skills_project/Assets/Scripts/TextController.cs
+++ NEA_Skills_project/Assets/Scripts/TextController.cs
@@ -6,17 +6,29 @@ using UnityEngine;
 public class TextController : MonoBehaviour
 {
     public TextMeshPro mushroomCounter;
-    GameObject[] totalMushrooms;   
+    public TextMeshPro livesCounter;
+    public TextMeshPro gameTimer;
+    GameObject[] totalMushrooms;
+    int lives;
+    GameObject Dino;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Dino = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
         totalMushrooms = GameObject.FindGameObjectsWithTag("MushroomRed");
+
         mushroomCounter.text = "Mushrooms: " + totalMushrooms.Length.ToString();
+
+        lives = Dino.GetComponent<DinoMovement>().lives;
+
+        livesCounter.text = "Lives: " + lives;
+
+        gameTimer.text = "Time: " + Time.timeSinceLevelLoad.ToString("0.00");
     }
+    
 }
