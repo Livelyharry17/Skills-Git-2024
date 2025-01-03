646e45fc78690ddc1b8379f0ebf9785b48997a91
Livelyharry17 <Livelyharry17@gmail.com>
2025-01-03 16:52:27 +0000

    End of task 5b

---
+++ NEA_Skills_project/Assets/Scripts/Navigation.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Navigation : MonoBehaviour
+{
+    private Canvas mainCamera;
+    public TextMeshPro timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer.text = Time.fixedTime.ToString("0.00");
+    }
+
+    public void ClickedButtonA()
+    {
+        Debug.Log("Button A Clicked!");
+    }
+    public void ClickedButtonB()
+    {
+        Debug.Log("Button B Clicked!");
+    }
+    public void ClickedExit()
+    {
+        Debug.Log("Exit Clicked!");
+    }
+}
